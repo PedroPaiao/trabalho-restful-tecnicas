66327c414e6b4e8e737a96ac05ec976f927a3882
Pedro Paião <devpedropaiao@gmail.com>
2022-05-21 17:27:51 -0400

    add enum to situacao

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -2,4 +2,9 @@ class Aluno < ApplicationRecord
   RGA_REGEX = /([0-9]{4}[\.]?[0-9]{4}?[0-9]{3}[\.]?[-][0-9]{1})/
 
   validates :rga, format: { with: RGA_REGEX }, length: { maximum: 15 }
+
+  enum situacao: {
+    ativo: 0,
+    inativo: 1
+  }
 end
